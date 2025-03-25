a2b99e9a976b356ed41e2a6ffd4a694bae37f1ed
MobinX <mobin0219@gmail.com>
2025-03-25 06:56:16 -0400

    Alhamdulillah

--- app/TimeLine.tsx
+++ app/TimeLine.tsx
@@ -1,6 +1,8 @@
 import { Container } from "components/Container";
+import { ChevronDownIcon } from "components/Icons";
 import Timeline, { TimelineProps } from "components/timeline";
 import { timelineGroups } from "content/timeline";
+import Link from "next/link";
 
 
 
@@ -12,6 +14,15 @@ export const TimelineComponent = () => {
                     Events
                 </h3>
                 <Timeline timelineData={timelineGroups} />
+                <div className="flex justify-center mt-0">
+                    <Link
+                        href="/events"
+                        className="group flex items-center text-sm font-medium text-zinc-800 dark:text-zinc-200 hover:text-teal-500 dark:hover:text-teal-500"
+                    >
+                        Show more
+                        <ChevronDownIcon className="ml-3 h-auto w-[10px] stroke-zinc-500 group-hover:stroke-teal-500 dark:group-hover:stroke-teal-500" />
+                    </Link>
+                </div>
             </div>
         </Container>
     )
